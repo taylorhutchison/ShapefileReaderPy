963ab2c9886bb152074c447267413036dcf71788
Taylor Hutchison <seanthutchison@gmail.com>
2015-04-11 10:06:25 -0500

    (documentation): Added block comments for all the methods in the ShapefileReader class to help explain their purpose.

--- ShapefileReader.py
+++ ShapefileReader.py
@@ -1,5 +1,5 @@
 """
-This script exposed a class used to read the shapefile format.
+This script exposes a class used to read the shapefile format.
 It is read according to the standard published by
 ESRI at http://www.esri.com/library/whitepapers/pdfs/shapefile.pdf.
 The class is Shapefile, which when instantiated is pointed to the
@@ -40,7 +40,9 @@ class Shapefile:
     # The shapefile type (i.e. point, polyline, etc.) as a ShapeType Enum
     Shapetype = None
 
-    @staticmethod
+    # A shapefile has a single shape type defined as an integer from bytes 32 thru 36 in the main file header.
+    # The integer corresponds to pre-defined shape types. This Enum makes dealing with the shape type easier by
+    # not having to deal directly with the integer representation.
     class Shape(Enum):
         Null = 0
         Point = 1
@@ -57,6 +59,8 @@ class Shapefile:
         MultiPointM = 28
         MultiPatch = 31
 
+    # This method takes all the main record bytes and transforms them into an array of x, y pairs.
+    # An example resulting structure is [ [x,y], [x,y], [x,y] ], which represents three distinct point features.
     @staticmethod
     def __bytes_to_point_records(file_bytes):
         point_records = []
@@ -68,6 +72,9 @@ class Shapefile:
             byte_position += 28
         return point_records
 
+    # This method takes all the main record bytes and transforms them into an array of multipoint sets.
+    # An example resulting structure is [ [ [x,y], [x,y], [x,y] ], [ [x,y], [x,y] ] ], which represents two
+    # distinct features, the first feature has three points, and the second feature has two points.
     @staticmethod
     def __bytes_to_multipoint_records(file_bytes):
         multipoint_records = []
@@ -84,6 +91,15 @@ class Shapefile:
             byte_position += (48 + (16 * num_points))
         return multipoint_records
 
+    # This method takes all the main record bytes and transforms them into an array of "polytype" sets.
+    # Polygons are nearly identical to Polylines, except that they are "closed" meaning the final point
+    # in the series of points for each feature is identical to the starting point. It is possible to
+    # create both Polyline and Polygon types using the same method, because their structure is so similar.
+    # Polylines and Polygons can have multiple parts per features, and therefore all the points in a feature
+    # may not be connected.
+    # An example resulting structure is [ [ [ [x,y], [x,y], [x,y] ], [ [x,y], [x,y] ] ] ], which represents a
+    # single polyline feature with two parts. The first part has three connected points, and the second part has
+    # two connected points.
     @staticmethod
     def __bytes_to_polytype_records(file_bytes):
         polyline_records = []
@@ -113,6 +129,11 @@ class Shapefile:
             byte_position += 52 + (4 * num_parts) + (num_points * 16)
         return polyline_records
 
+    # The first 100 bytes of a .shp file contains the "main file header". Which is essentially some
+    # metadata about the shape type, file length, and bounding information. There is also information
+    # about the version and filecode, which are always 1000 and 9994 respectively.
+    # The first 28 bytes of the header are in big endian notation, and the rest are in little endian.
+    # For more information consult the specification at http://www.esri.com/library/whitepapers/pdfs/shapefile.pdf
     def __bytes_to_header(self, file_bytes):
         self.Header['filecode'] = int.from_bytes(file_bytes[0:4], byteorder='big')
         self.Header['length'] = int.from_bytes(file_bytes[24:28], byteorder='big')
@@ -128,6 +149,7 @@ class Shapefile:
         self.Header['mmax'] = struct.unpack("<d", file_bytes[92:100])[0]
         self.Shapetype = Shapefile.Shape(self.Header['type'])
 
+    # A wrapper method that takes the shape type and calls the appropriate method.
     def __bytes_to_records(self, file_bytes):
         if self.Shapetype == Shapefile.Shape.Null:
             self.Records = [None] * (file_bytes / 4)
@@ -138,6 +160,8 @@ class Shapefile:
         elif self.Shapetype == Shapefile.Shape.Polyline or self.Shapetype == Shapefile.Shape.Polygon:
             self.Records = Shapefile.__bytes_to_polytype_records(file_bytes)
 
+    # The only publicly exposed method of the class. This reads the shapefile and stores the main header and
+    # record information in the Header and Records properties respectively.
     def read(self):
         with open(self.Path, 'rb') as shpfile:
             self.__bytes_to_header(shpfile.read(100))
