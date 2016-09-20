39ce173564b9c6efa601d575efe737d5d5a7ace9
Taylor Hutchison <seanthutchison@gmail.com>
2016-09-19 22:40:05 -0500

    Started work on Dbase 5 parsing

--- DbaseReader.py
+++ DbaseReader.py
@@ -28,6 +28,29 @@ __status__ = 'Development'
 
 class Dbase:
 
+    Path = None
+
+    def read(self):
+         with open(self.Path, 'rb') as dbasefile:
+             file_bytes = dbasefile.read(800)
+             version = int.from_bytes(file_bytes[0:1], byteorder='big')
+             dbase_version = int(str((version >> 0) & 1) + str((version >> 1) & 1),2)
+             year = (int.from_bytes(file_bytes[1:2], byteorder='little'))
+             month = (int.from_bytes(file_bytes[2:3], byteorder='little'))
+             day = (int.from_bytes(file_bytes[3:4], byteorder='little'))
+             record_count = int.from_bytes(file_bytes[4:8], byteorder='little')
+             header_bytes = int.from_bytes(file_bytes[8:10], byteorder='little')
+             record_bytes = int.from_bytes(file_bytes[10:12], byteorder='little')
+             for i in range(0,20):
+                jump = 32
+                start = 30 + (jump * i)
+                first_record = file_bytes[start:(start+jump)]
+                name = ''
+                for b in first_record[0:11]:
+                    name = name + chr(b)
+                print(name)
+
+
     def __init__(self, path=None):
         if path and os.path.exists(path) and os.path.splitext(path)[1] == '.dbf':
             self.Path = path
